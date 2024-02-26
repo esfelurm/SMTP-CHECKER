d11a355bbb3cf46f3dd2fb44855b991688f821c3
EsfeLurM <104654028+esfelurm@users.noreply.github.com>
2024-02-26 09:16:18 +0330

    Update ComboEdit.py

--- ComboEdit.py
+++ ComboEdit.py
@@ -1,8 +1,8 @@
 m = input("Address Combo : ")
+m = open(m,'r')
 host = input("Enter the desired domain : ")
 port = input("PORT : ")
-with open('COMBONEW','w') as p:
+with open('/sdcard/COMBONEW.txt','w') as p:
     for mk in m:
         mk = mk.strip().replace(':','|')
         p.write(f"{host}|{port}|{mk}\n")
-m.close()
